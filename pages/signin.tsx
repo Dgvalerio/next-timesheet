68e8f96b9bfe95b83b9c192a330e67f9c0359ce0
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-30 22:37:07 -0300

    :sparkles: feat(pages): add SignIn page

---
+++ pages/signin.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const SignIn: NextPage = () => (
+  <>
+    <Head>
+      <title>Next Timesheet - SignIn</title>
+    </Head>
+
+    <main>
+      <h1>SignIn Page</h1>
+    </main>
+  </>
+);
+
+export default SignIn;
